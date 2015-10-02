e2bd9fd04c7dc5c391f927fdb53152120c8881c0
Gabe Austin <gabriel.austin1@gmail.com>
2015-10-02 00:41:22 -0500

    fix font awesome

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module MainSite
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << Rails.root.join("medias", "images", "thumbnails")
+
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
